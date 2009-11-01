fd57af01bf6f68ac0a698a60f858f57b96b980b9
Fabian <fabian@wow-extrem.de>
2009-11-01 13:55:53 +0100

    [298] Typo.

--- src/game/MovementHandler.cpp
+++ src/game/MovementHandler.cpp
@@ -278,12 +278,10 @@ void WorldSession::HandleMoveWorldportAckOpcode()
     {
 		GetPlayer()->ResetMap();
 		
-		sLog.outDebug("WORLD: teleport of player %s (%d) to location %d, %f, %f, %f, %f failed", GetPlayer()->GetName(), GetPlayer()->GetGUIDLow(), loc.mapid, loc.coord_x, loc.coord_y, loc.coord_z, loc.orientation);
-
 		if(GetPlayer()->TeleportTo(GetPlayer()->m_homebindMapId, GetPlayer()->m_homebindX, GetPlayer()->m_homebindY, GetPlayer()->m_homebindZ, GetPlayer()->GetOrientation()))
 		{
 			sLog.outError("WORLD: failed to teleport player %s (%d) to map %d because of unknown reason!", GetPlayer()->GetName(), GetPlayer()->GetGUIDLow(), loc.GetMapId());
-			assert(false;
+			assert(false);
 		}
 		return;
 	}

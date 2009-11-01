b446115cd80ceefafb261b526bc198fe22be7d2a
Fabian <fabian@wow-extrem.de>
2009-11-01 19:54:16 +0100

    [332] Implement proper creating mail text copy item from mail template based mail.

--- sql/updates/331_01_mail_loot_template.sql
+++ sql/updates/331_01_mail_loot_template.sql
@@ -3,4 +3,5 @@
 RENAME TABLE quest_mail_loot_template TO mail_loot_template;
 
 UPDATE mail_loot_template, quest_template
-  SET entry = quest_template.RewMailTemplateId WHERE mail_loot_template.entry = quest_template.entry;
+  SET mail_loot_template.entry = quest_template.RewMailTemplateId WHERE mail_loot_template.entry = quest_template.entry;
+
